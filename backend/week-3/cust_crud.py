ef48792c4a9525b3bbad995ca5fa341eb47f4921
Aditya Bansal <adityabansal29@gmail.com>
2024-07-30 15:49:14 +0530

    Backend week 3 staff cust CRUD

--- backend/week-3/cust_crud.py
+++ backend/week-3/cust_crud.py
@@ -4,8 +4,17 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
+conn = psycopg2.connect("dbname=DSOC user=postgres password=Ab290805")
+cur = conn.cursor()
 
-#customer CRUD Operations
+login_manager = LoginManager()
+# cur.execute(''' alter table Staff add column is_approved boolean''')
+# cur.execute(''' alter table Staff add column s_password varchar(100)''')
+# cur.execute(''' alter table Customer add column c_password varchar(100)''')
+
+
+
+#staff CRUD Operations
 app.secret_key = '12d'
 def db_connection():
     conn = psycopg2.connect("dbname=DSOC user=postgres password=Ab290805")
@@ -13,7 +22,7 @@ def db_connection():
 
 @app.route("/")
 def hello():
-    return redirect(url_for('retrieve_staff'))
+    return render_template('index.html')
 
 
 @app.route("/staff/create/", methods=['GET','POST'])
@@ -95,7 +104,7 @@ def update_staff(s_ID):
     return render_template('update_staff.html', staff = staff)
 
 
-@app.route("/store/delete/<s_ID>/", methods = ['POST'])
+@app.route("/staff/delete/<s_ID>/", methods = ['POST'])
 def delete_staff(s_ID):
     conn = db_connection()
     cur = conn.cursor()
@@ -106,6 +115,97 @@ def delete_staff(s_ID):
     conn.close()
     flash(f'Staff deleted successfully! {del_item}', 'success')
     return redirect(url_for('retrieve_staff'))
+
+
+
+
+#customer CRUD Operations
+@app.route("/customer/create/", methods=['GET','POST'])
+def create_customer():
+    if request.method == 'POST':
+        c_ID = request.form['c_ID']
+        c_name = request.form['c_name']
+        c_email = request.form['c_email']
+        c_contact = request.form['c_contact']
+        c_password = request.form['c_password']
+        conn = db_connection()
+        cur = conn.cursor()
+        cur.execute(''' insert into Customer values (%s, %s, %s, %s, %s)'''
+                    ,(c_ID, c_name, c_email, c_contact, c_password,))
+        conn.commit()
+        cur.close()
+        conn.close()
+        flash('Customer created successfully!', 'success')
+        return redirect(url_for('retrieve_customer'))
+    return render_template('create_customer.html')
+
+
+@app.route("/customer/retrieve/", defaults={'c_ID': None})
+@app.route("/customer/<c_ID>/")
+def retrieve_customer(c_ID):
+    conn = db_connection()
+    cur = conn.cursor()
+
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 10, type=int)
+    search_query = request.args.get('search', '')
+    sort_by = request.args.get('sort_by', 'c_ID')
+    sort_order = request.args.get('sort_order', 'asc')
+
+    if c_ID:
+        cur.execute('''select * from Customer where c_ID = %s''', (c_ID,))
+        customer = cur.fetchone()
+
+    else:
+        query = f"select * from Customer where c_ID ilike %s order by {sort_by} {sort_order} limit %s offset %s"
+        cur.execute(query, (f'%{search_query}%', per_page, (page-1)*per_page,))
+        customer = cur.fetchall()
+
+    cur.execute('''select count(*) from Customer where c_ID ilike %s''', (f'%{search_query}%',))
+    total_customer = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return render_template('retrieve_customer.html', customer_list=customer, page=page, per_page=per_page, total_items=total_customer, search_query=search_query, sort_by=sort_by, sort_order=sort_order)
+
+
+@app.route("/customer/update/<c_ID>/", methods = ['GET','POST'])
+def update_customer(c_ID):
+    conn = db_connection()
+    cur = conn.cursor()
+    cur.execute('''select * from Customer where c_ID = %s''', (c_ID,))
+    customer = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    if request.method == 'POST':
+        c_ID2 = request.form['c_ID']
+        c_name = request.form['c_name']
+        c_email = request.form['c_email']
+        c_contact = request.form['c_contact']
+        c_password = request.form['c_password']
+        conn = db_connection()
+        cur = conn.cursor()
+        cur.execute('''update Customer set c_ID = %s, c_name = %s, c_email = %s, c_contact = %s, c_password = %s where c_ID = %s''',
+                    (c_ID2, c_name, c_email, c_contact, c_password, c_ID,))
+        conn.commit()
+        cur.close()
+        conn.close()
+        flash('Customer updated successfully!', 'success')
+        return redirect(url_for('retrieve_customer'))
+    return render_template('update_customer.html', customer = customer)
+
+
+@app.route("/customer/delete/<c_ID>/", methods = ['POST'])
+def delete_customer(c_ID):
+    conn = db_connection()
+    cur = conn.cursor()
+    cur.execute('''delete from Customer where c_ID = %s returning *''', (c_ID,))
+    del_item = cur.fetchone()
+    conn.commit()
+    cur.close()
+    conn.close()
+    flash(f'Customer deleted successfully! {del_item}', 'success')
+    return redirect(url_for('retrieve_customer'))
 
 
 if __name__ == '__main__':
